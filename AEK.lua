d9d3bc955691867616984cc46bd476454a25a8db
AEKANO <79565951+AEKANO@users.noreply.github.com>
2021-03-22 13:58:51 +0300

    Add files via upload

--- AEK.lua
+++ AEK.lua
@@ -9544,7 +9544,7 @@ if SecondSudo(msg) then
 if text == "تحديث السورس" or text == "تحديث سورس" then 
 Dev_Aek(msg.chat_id_, msg.id_, 1, '❦ ⁞ جاري تحديث سورس ايكان', 1, 'md') 
 os.execute('rm -rf AEK.lua') 
-os.execute('wget https://raw.githubusercontent.com/AEKOTEAM/AEK/master/AEK.lua') 
+os.execute('wget https://raw.githubusercontent.com/AEKANO/AEK/master/AEK.lua') 
 dofile('AEK.lua') 
 io.popen("rm -rf ../.telegram-cli/*")
 print("\27[31;47m\n          ( تم تحديث السورس )          \n\27[0;34;49m\n") 
@@ -9633,7 +9633,7 @@ end
 send(msg.chat_id_, msg.id_,Files)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/AEKOTEAM/AEKFiles/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/AEKANO/AEKFiles/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -9665,7 +9665,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/AEKOTEAM/AEKFiles/master/AEKFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/AEKANO/AEKFiles/master/AEKFiles/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n❦ ⁞ الملف ↫ *"..FileName.."*\n❦ ⁞ تم تعطيله وحذفه من البوت بنجاح") 
@@ -9677,7 +9677,7 @@ end
 if text and text:match("^(تفعيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/AEKOTEAM/AEKFiles/master/AEKFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/AEKANO/AEKFiles/master/AEKFiles/"..FileName)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..FileName,'w+')
 ChekAuto:write(GetJson)
